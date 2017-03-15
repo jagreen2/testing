b4c76ca3dc8fbabc9249655c292cebe572260a73
Alexis Green <jalexis@jalexis.net>
2017-03-15 14:51:24 -0700

    First commit

---
+++ test.js
@@ -0,0 +1,24 @@
+console.log("Hello World")
+
+function add(a,b) {
+	return a+b;
+}
+
+function subtract(a,b) {
+	return a - b;
+}
+
+function multiply(a,b) {
+	return a * b;
+}
+
+var test = add(4,3);
+// console.log(test);
+
+var y = "Hello     ";
+var x = y.length;
+console.log(y.trim().length);
+
+// console.log("indexOf", y.toLowerCase().indexOf("h"));
+
+
